404b106d74fadba6fa35ef4a28b7d98d3ccbae18
mastermind-fa <153628881+mastermind-fa@users.noreply.github.com>
2024-09-28 19:24:53 +0600

    Update cinema.py

--- cinema.py
+++ cinema.py
@@ -2,38 +2,38 @@
 ##Name - Farhana Alam
 
 class Star_Cinema:
-    _hall_list=[]
+    __hall_list=[]
     def entry_hall(self,hall):
-        self._hall_list.append(hall)
+        self.__hall_list.append(hall)
         
         
         
 class Hall(Star_Cinema):
     def __init__(self,rows,cols,hall_no):
         super().__init__()
-        self._seats={}
-        self._show_list=[]
-        self._rows=rows
-        self._cols=cols
-        self._hall_no=hall_no
+        self.__seats={}
+        self.__show_list=[]
+        self.__rows=rows
+        self.__cols=cols
+        self.__hall_no=hall_no
         
     def entry_show(self,id,movie_name,time):
         info=(id,movie_name,time)
-        self._show_list.append(info)
-        self._seats[id]=[[0 for i in range(self._cols)] for j in range(self._rows)]
+        self.__show_list.append(info)
+        self.__seats[id]=[[0 for i in range(self.__cols)] for j in range(self.__rows)]
         
         
     def book_seats(self,show_id,seat_list):
-        if show_id not in self._seats:
+        if show_id not in self.__seats:
             print("Show not found. Try again")
             return
         
         for row, col in seat_list:
-            if row < 0 or row >= self._rows or col < 0 or col >= self._cols:
+            if row < 0 or row >= self.__rows or col < 0 or col >= self.__cols:
                 print(f"Error: Seat ({row},{col}) is invalid.")
                 continue
-            if self._seats[show_id][row][col] == 0:
-                self._seats[show_id][row][col] = 1  
+            if self.__seats[show_id][row][col] == 0:
+                self.__seats[show_id][row][col] = 1  
                 print(f"Seat no. ({row}, {col}) booked successfully.")
             else:
                 print(f"Sorry!!! Seat ({row}, {col}) is already booked.")
@@ -42,23 +42,22 @@ class Hall(Star_Cinema):
     def view_show_list(self):
         print("Now premiering:")
         print("\tID\tName\t\tTime")
-        for movie in self._show_list:
+        for movie in self.__show_list:
             print(f"\t{movie[0]}\t{movie[1]}\t{movie[2]}")
             
     def view_available_seats(self, show_id):
-        if show_id not in self._seats:  
+        if show_id not in self.__seats:  
             print("Show not found. Try again")  
             return
         
         print("Available seats are:")
         ok= True
-        for row in range(self._rows):
-            for col in range(self._cols):
-                if self._seats[show_id][row][col] == 0:
+        for row in range(self.__rows):
+            for col in range(self.__cols):
+                if self.__seats[show_id][row][col] == 0:
                     ok= False  
                     print(f"({row},{col})")
         if ok:
             print("No seats available")
 
        
-
